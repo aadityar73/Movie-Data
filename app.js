0a41930d4d5d1de0cb7b6f11e7f96cf0c237ff39
Aaditya Raikar <aadityaraikar73@gmail.com>
2023-04-10 02:55:27 +0530

    v

---
+++ app.js
@@ -0,0 +1,57 @@
+let API = "http://www.omdbapi.com/?apikey=b2647f6f&t=";
+let loadingStatus = false;
+let loader = document.getElementById("loader");
+let movieName = document.getElementById("movieName");
+let movieContainer = document.getElementById("movieContainer");
+let errorContainer = document.getElementById("errorContainer");
+movieContainer.classList.add("d-none");
+errorContainer.classList.add("d-none");
+
+function renderMovie() {
+  if (loadingStatus == true) {
+    loader.classList.add("loader");
+  } else {
+    loader.classList.remove("loader");
+  }
+}
+
+function fetchMovieDetails() {
+  let apiQuery = API + movieName.value;
+  loadingStatus = true;
+  renderMovie();
+  fetch(apiQuery)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.Error != "Movie not found!") {
+        movieContainer.classList.remove("d-none");
+        errorContainer.classList.add("d-none");
+
+        title.innerText = data.Title;
+        genre.innerText = data.Genre;
+        movieImg.src = data.Poster;
+        desc.innerText = data.Plot;
+        actors.innerText = data.Actors;
+        director.innerText = data.Director;
+        imdb.innerText = data.Ratings[0].Value;
+        awards.innerText = data.Awards;
+        boxOffice.innerText = data.BoxOffice;
+        movieTime.innerText = data.Runtime;
+        writer.innerText = data.Writer;
+        releaseDate.innerText = data.Released;
+
+        movieName.value = "";
+        console.log(data);
+
+        loadingStatus = false;
+        renderMovie();
+      } else {
+        movieContainer.classList.add("d-none");
+        errorContainer.classList.remove("d-none");
+        loadingStatus = false;
+        renderMovie();
+        movieName.value = "";
+      }
+    });
+}

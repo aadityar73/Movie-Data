12930dee5f7ed595adc68c304a6bcd0f7a7f9a16
Aaditya Raikar <aadityaraikar73@gmail.com>
2023-04-16 13:58:51 +0530

    v1

--- app.js
+++ app.js
@@ -19,39 +19,46 @@ function fetchMovieDetails() {
   let apiQuery = API + movieName.value;
   loadingStatus = true;
   renderMovie();
+
   fetch(apiQuery)
     .then((response) => {
+      console.log(response);
       return response.json();
     })
     .then((data) => {
-      if (data.Error != "Movie not found!") {
-        movieContainer.classList.remove("d-none");
-        errorContainer.classList.add("d-none");
-
-        title.innerText = data.Title;
-        genre.innerText = data.Genre;
-        movieImg.src = data.Poster;
-        desc.innerText = data.Plot;
-        actors.innerText = data.Actors;
-        director.innerText = data.Director;
-        imdb.innerText = data.Ratings[0].Value;
-        awards.innerText = data.Awards;
-        boxOffice.innerText = data.BoxOffice;
-        movieTime.innerText = data.Runtime;
-        writer.innerText = data.Writer;
-        releaseDate.innerText = data.Released;
-
-        movieName.value = "";
-        console.log(data);
-
+      if (movieName.value == "") {
         loadingStatus = false;
         renderMovie();
       } else {
-        movieContainer.classList.add("d-none");
-        errorContainer.classList.remove("d-none");
-        loadingStatus = false;
-        renderMovie();
-        movieName.value = "";
+        if (data.Error == "Movie not found!") {
+          movieContainer.classList.add("d-none");
+          errorContainer.classList.remove("d-none");
+          loadingStatus = false;
+          renderMovie();
+          movieName.value = "";
+        } else {
+          movieContainer.classList.remove("d-none");
+          errorContainer.classList.add("d-none");
+
+          title.innerText = data.Title;
+          genre.innerText = data.Genre;
+          movieImg.src = data.Poster;
+          desc.innerText = data.Plot;
+          actors.innerText = data.Actors;
+          director.innerText = data.Director;
+          imdb.innerText = data.Ratings[0].Value;
+          awards.innerText = data.Awards;
+          boxOffice.innerText = data.BoxOffice;
+          movieTime.innerText = data.Runtime;
+          writer.innerText = data.Writer;
+          releaseDate.innerText = data.Released;
+
+          movieName.value = "";
+          console.log(data);
+
+          loadingStatus = false;
+          renderMovie();
+        }
       }
     });
 }
